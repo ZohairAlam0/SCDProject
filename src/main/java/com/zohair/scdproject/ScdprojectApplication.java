af06a610904555397cd5add685dacc0a856d260c
Crizax <zohairalam03@gmail.com>
2024-11-08 01:19:24 +0500

    Initial commit

---
+++ src/main/java/com/zohair/scdproject/ScdprojectApplication.java
@@ -0,0 +1,13 @@
+package com.zohair.scdproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScdprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScdprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zohair/scdproject/ScdprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zohair.scdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScdprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
